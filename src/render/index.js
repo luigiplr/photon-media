e7ed29c0eb85877a58665c4e30633b19c6a73e3d
Luigi Poole <luigipoole@outlook.com>
2016-03-11 01:26:41 -0800

    renamings

--- src/render/index.js
+++ src/render/index.js
@@ -2,9 +2,6 @@ import reactPolymer from 'react-polymer'
 import React from 'react'
 import { render } from 'react-dom'
 import Framework from './components/Framework.react'
-import './app.global.css'
-
+import './app.reset.css'
 
 render(<Framework />, document.getElementById('root'))
-
-if (process.env.NODE_ENV !== 'production') {}
