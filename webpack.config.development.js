a4a8861580847acba558df0f24ce4f96f9b978c2
Luigi Poole <luigipoole@outlook.com>
2016-03-11 01:03:40 -0800

    one more

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -1,23 +1,23 @@
 /* eslint strict: 0 */
 'use strict';
 
-const webpack = require('webpack');
-const webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
-const baseConfig = require('./webpack.config.base');
+const webpack = require('webpack')
+const webpackTargetElectronRenderer = require('webpack-target-electron-renderer')
+const baseConfig = require('./webpack.config.base')
 
 
-const config = Object.create(baseConfig);
+const config = Object.create(baseConfig)
 
-config.debug = true;
+config.debug = true
 
-config.devtool = 'cheap-module-eval-source-map';
+config.devtool = 'cheap-module-eval-source-map'
 
 config.entry = [
     'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
     './src/render'
 ];
 
-config.output.publicPath = 'http://localhost:3000/dist/';
+config.output.publicPath = 'http://localhost:3000/dist/'
 
 config.module.loaders.push({
     test: /\.global\.css$/,
@@ -31,8 +31,7 @@ config.module.loaders.push({
         'style-loader',
         'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
     ]
-});
-
+})
 
 config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
@@ -43,8 +42,8 @@ config.plugins.push(
             NODE_ENV: JSON.stringify('development')
         }
     })
-);
+)
 
-config.target = webpackTargetElectronRenderer(config);
+config.target = webpackTargetElectronRenderer(config)
 
-module.exports = config;
+module.exports = config
