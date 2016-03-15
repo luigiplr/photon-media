48e488c70cef7cffb103a7d243cb2a733e0f4a0c
Luigi Poole <luigipoole@outlook.com>
2016-03-15 10:35:59 -0700

    further work on trakt

--- src/main/initWorkers.js
+++ src/main/initWorkers.js
@@ -1,4 +1,5 @@
 import getPort from 'get-port'
+import socketIO from 'socket.io'
 import path from 'path'
 import Worker from 'workerjs'
 
@@ -8,17 +9,34 @@ export default class initWorkers {
     constructor() {
         getPort().then(port => {
             this.port = port
-            console.log(port)
+            this.initSocketServer()
             this.trakt()
         })
     }
 
+    initSocketServer() {
+        this.socketServer = socketIO()
+        this.socketServer.on('connection', socket => {
+           // console.log(socket)
+        })
+        this.socketServer.listen(this.port)
+    }
+
     trakt() {
-        console.log('that thing!')
-        const traktWorkerPath = path.join(__dirname, 'workers', 'trakt.worker.js')
-        console.log(traktWorkerPath)
-        this.traktWorker = new Worker(traktWorkerPath)
-        console.log(this.traktWorker)
+        this.traktWorker = new Worker(path.join(__dirname, 'workers', 'trakt.worker.js'), true)
         this.traktWorker.postMessage(this.port)
+        this.traktWorker.onmessage = ({ data }) => {
+            const { type, message } = data
+
+            switch (type) {
+                case 'info':
+                case 'log':
+                    console.log('Trakt Worker log:', message)
+                    break
+                case 'error':
+                    console.error('Trakt Worker error:', message)
+                    break
+            }
+        }
     }
 }
--- src/main/workers/trakt.worker.js
+++ src/main/workers/trakt.worker.js
@@ -1,3 +1,32 @@
-self.onmessage = ({ data }) => {
-    self.postMessage('received ' + data)
+import socketClient from 'socket.io-client'
+
+console.log = () => self.postMessage({
+    type: 'log',
+    message: arguments
+})
+
+self.onmessage = ({ data }) => new trakt(data) // init socket connection when we get the message
+
+
+class trakt {
+    constructor(port) {
+        this.socket = socketClient(`http://localhost:${port}`)
+
+        this.socket.on('connect', () => self.postMessage({
+            type: 'info',
+            message: 'Socket connected!'
+        }))
+        this.socket.on('disconnect', () => self.postMessage({
+            type: 'info',
+            message: 'Socket disconnected!'
+        }))
+
+        this.initEvents()
+    }
+
+    initEvents() {
+        this.socket.on('trakt:fetch', ({ type, data }) => {
+
+        })
+    }
 }
