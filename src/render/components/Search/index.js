7a29c8bd8378487488c49450786f729adb429646
Luigi Poole <luigipoole@outlook.com>
2016-03-18 00:50:01 -0700

    add backdrop source

--- src/render/components/Search/index.js
+++ src/render/components/Search/index.js
@@ -14,23 +14,17 @@ export default class Search extends Component {
         return (
             <div className="search-container">
                 <div style={{backgroundImage: 'url(https://walter.trakt.us/images/movies/000/128/378/fanarts/original/401b051d49.jpg)'}} className="search-container-backdrop"/>
-                <div className="top-button-container">
-                    <paper-icon-button className="bottom-btn" icon="link"/>
+                <div className="bottom-button-container">
                     <paper-icon-button className="bottom-btn" icon="settings"/>
+                </div>
+                <div className="bottom-info-container">
+                    <h1 className="title">Spectre</h1><span className="year">2015</span>
+                    <p className="rating">PG-13</p>
                 </div>
                 <div id="wrapper" style={{textAlign: 'center'}}>
                     <paper-material className="search-box-contain" elevation="1">
-                        <paper-menu-button>
-                            <paper-icon-button icon="more-vert" className="dropdown-trigger"/>
-                            <paper-menu className="dropdown-content">
-                                <paper-item>All</paper-item>
-                                <paper-item>Movies</paper-item>
-                                <paper-item>Series</paper-item>
-                                <paper-item>Music</paper-item>
-                            </paper-menu>
-                        </paper-menu-button>
                         <input className="searchtext" />
-                        <paper-icon-button className="search-btn" icon="search"/>
+                        <paper-icon-button className="search-btn" icon="link"/>
                     </paper-material>
                 </div>
             </div>
