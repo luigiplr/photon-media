c878f18688b77dc1187e60ef9395ebea2cac863c
Luigi Poole <luigipoole@outlook.com>
2016-03-18 02:01:52 -0700

    get window controls working

--- src/render/components/Header/index.js
+++ src/render/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'React'
+import { remote } from 'electron'
 
 
 export default class Header extends Component {
@@ -7,9 +8,9 @@ export default class Header extends Component {
             <header>
                 <span className="title">Photon Media</span>
                 <div className="os-controls">
-                    <paper-icon-button className="minimize" icon="remove"/>
-                    <paper-icon-button className="maximize" icon="check-box-outline-blank"/>
-                    <paper-icon-button className="close" icon="close"/>
+                    <paper-icon-button onClick={() => remote.getCurrentWindow().minimize()} className="minimize" icon="remove"/>
+                    <paper-icon-button onClick={() => (remote.getCurrentWindow().isMaximized() ? remote.getCurrentWindow().unmaximize() : remote.getCurrentWindow().maximize())} className="maximize" icon="check-box-outline-blank"/>
+                    <paper-icon-button onClick={() => remote.getCurrentWindow().close()} className="close" icon="close"/>
                 </div>
             </header>
         )
