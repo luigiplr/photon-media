5d733527e55ba187e22b0a0996fb8dce9d6af91b
Luigi Poole <luigipoole@outlook.com>
2016-03-18 01:17:28 -0700

    work on trakt

--- src/main/workers/trakt.worker.js
+++ src/main/workers/trakt.worker.js
@@ -1,10 +1,12 @@
 import socketClient from 'socket.io-client'
 import request from 'request'
+import traktAPI from 'trakt-api'
 
 self.onmessage = ({ data }) => new trakt(data) // init socket connection when we get the message (port)
 
 class trakt {
     constructor(port) {
+        this.trakt = traktAPI('021d98ace731891d1d77ae3ae380c84f8cfe17b0f1960de5148535ab049d20a7')
         this.socket = socketClient(`http://localhost:${port}`)
 
         this.socket.on('connect', () => this.log('Socket Connected!'))
@@ -20,6 +22,9 @@ class trakt {
     });
 
     initEvents() {
+
+        this.socket.on('trakt:search', ({ id, params }) => this.trakt.search(...params, results => this.socket.emit(`trakt:search:response`, { id, results })))
+
         this.socket.on('trakt:fetch', ({ type, data }) => {
 
         })
