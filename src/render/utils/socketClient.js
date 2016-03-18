fc12f42788855572bcf8801f34c71f2b529af833
Luigi Poole <luigipoole@outlook.com>
2016-03-18 00:00:13 -0700

    more info

--- src/render/utils/socketClient.js
+++ src/render/utils/socketClient.js
@@ -7,10 +7,14 @@ class photonSocketClient extends EventEmitter {
     constructor() {
         super()
 
-        if (!process.env.WORKERS_PORT)
+        if (!process.env.WORKERS_PORT) {
+            console.info('Workers Port not detected, waiting for main thread event')
             ipcRenderer.on('workers:port', port => this.initSockets(port))
-        else
+        } else {
+            console.info('Workers Port detected, connecting to sockets immediately')
             this.initSockets(process.env.WORKERS_PORT)
+        }
+
     }
 
     initSockets(port) {
