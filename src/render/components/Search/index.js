95411bb9b005c9b12ba7ade4ba4b86877496ecac
Luigi Poole <luigipoole@outlook.com>
2016-03-19 02:03:59 -0700

    push crossfading

--- src/render/components/Search/index.js
+++ src/render/components/Search/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'React'
+import ReactCSSTransitionReplace from 'react-css-transition-replace'
 import { delay } from 'lodash'
 import { v4 as uuid } from 'node-uuid'
 
@@ -38,16 +39,25 @@ export default class Search extends Component {
             sockets.emit(`trakt:get:${type}`, { id: requestID, imdb: item[type].ids.imdb })
 
             this.props.workers.once(requestID, ({ images, certification, title, year, homepage }) => {
-                this.setState({
-                    backdrop: {
-                        homepage,
-                        image: images.fanart.full,
-                        certification,
-                        title,
-                        year
-                    }
-                })
-                delay(this._getNewBackdrop, 10000)
+
+                const backdropImage = new Image()
+
+                backdropImage.onload = () => {
+
+                    this.setState({
+                        backdrop: {
+                            homepage,
+                            image: images.fanart.full,
+                            certification,
+                            title,
+                            year
+                        }
+                    })
+                    delay(this._getNewBackdrop, 10000)
+                }
+
+                backdropImage.src = images.fanart.full
+
             })
         })
     };
@@ -55,14 +65,19 @@ export default class Search extends Component {
     render() {
         return (
             <div className="search-container">
-                <div style={{backgroundImage: `url(${this.state.backdrop.image})`}} className="search-container-backdrop"/>
+                <ReactCSSTransitionReplace className='transition-container' transitionName="cross-fade" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+                    <div key={this.state.backdrop.image} style={{backgroundImage: `url(${this.state.backdrop.image})`}} className="search-container-backdrop"/>
+                </ReactCSSTransitionReplace>
+                
                 <div className="bottom-button-container">
                     <paper-icon-button className="bottom-btn" icon="settings"/>
                 </div>
+
                 <div className="bottom-info-container">
                     <h1 className="title">{this.state.backdrop.title}</h1><span className="year">{this.state.backdrop.year}</span>
                     <p className="rating">{this.state.backdrop.certification}</p>
                 </div>
+
                 <div id="wrapper" style={{textAlign: 'center'}}>
                     <paper-material className="search-box-contain" elevation="1">
                         <input className="searchtext" />
