92cca17915147f2be2c1b73bb7c9f5658e270bfc
Luigi Poole <luigipoole@outlook.com>
2016-04-07 10:30:10 -0700

    cleaner

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -25,12 +25,8 @@ const electronDev = electronConnect.create({ path: 'build' })
 
 const uglifyOptions = {
     options: {
-        compress: {
-            screw_ie8: true
-        },
-        mangle: {
-            screw_ie8: true
-        }
+        compress: { screw_ie8: true },
+        mangle: { screw_ie8: true }
     }
 }
 
@@ -118,7 +114,7 @@ gulp.task('build-static-assets', () => {
         gulp.src('bower_components/**/*').pipe(gulp.dest('build/bower_components'))
 
     gulp.src('LICENSE').pipe(gulp.dest('build'))
-    
+
     gulp.src('src/main/**/*.html').pipe(htmlmin({ collapseWhitespace: true })).pipe(gulp.dest('build'))
     return gulp.src('src/images/**/*').pipe(gulp.dest('build/images'))
 })
