c98cf93559498778f7bc14978cf30b630a8dc311
Luigi Poole <luigipoole@outlook.com>
2016-04-16 01:39:08 -0700

    more synax

--- src/main/workers/engines.worker.js
+++ src/main/workers/engines.worker.js
@@ -15,29 +15,27 @@ workers.engines = class engineWorker {
     source: 'Engine Worker',
     type,
     message
-  });
+  })
 
   initEvents() {
     this.socket.on('engines:parse', ({ id, engines, type, value }) => Promise.all(engines.map(engine => this.spawnEngine(engine, type, value)))
       .then(compatible => this.socket.emit('engines', { id, data: compatible.filter(engine => engine !== false) })))
   }
 
-  spawnEngine(engine, type, value) {
-    return new Promise(resolve => {
-      let engineModule = require(engine.path)
-      const timeoutTimer = setTimeout(() => resolve(false), 30000)
-      engineModule = new engineModule({ type, value })
+  spawnEngine = (engine, type, value) => new Promise(resolve => {
+    let engineModule = require(engine.path)
+    const timeoutTimer = setTimeout(() => resolve(false), 30000)
+    engineModule = new engineModule({ type, value })
 
-      engineModule.once('incompatible', () => {
-        engineModule.removeAllListeners('parsed')
-        clearTimeout(timeoutTimer)
-        resolve(false)
-      })
-      engineModule.once('compatible', data => {
-        engineModule.removeAllListeners('incompatible')
-        clearTimeout(timeoutTimer)
-        resolve(data)
-      })
+    engineModule.once('incompatible', () => {
+      engineModule.removeAllListeners('parsed')
+      clearTimeout(timeoutTimer)
+      resolve(false)
     })
-  }
+    engineModule.once('compatible', data => {
+      engineModule.removeAllListeners('incompatible')
+      clearTimeout(timeoutTimer)
+      resolve(data)
+    })
+  })
 }
