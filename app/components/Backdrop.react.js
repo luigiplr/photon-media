2022c4a59ac21f2ead688a52aca641470d326f2e
Luigi Poole <luigipoole@outlook.com>
2016-06-09 15:59:42 -0700

    add check for info not enabled

--- app/components/Backdrop.react.js
+++ app/components/Backdrop.react.js
@@ -4,20 +4,14 @@ import styles from 'styles/Backdrop'
 
 
 export default class Backdrop extends Component {
-
-  state = {
-    trending: [],
-    backdrop: { image: '' }
-  };
-
-  defaultDelay = 20000;
-  initialDelay = 5000;
-
   static propTypes = {
     url: PropTypes.string,
     info: PropTypes.object,
     infoIsEnabled: PropTypes.bool.isRequired
   }
+
+  defaultDelay = 20000
+  initialDelay = 5000
 
   componentWillUnmount() {
     this.mounted = false
@@ -30,19 +24,23 @@ export default class Backdrop extends Component {
   _openBackDropURL = () => {
     if (!this.state.backdrop || !this.state.backdrop.homepage) return
     shell.openExternal(this.state.backdrop.homepage)
-  };
+  }
 
   render() {
-    const { info, url } = this.props
+    const { info, url, infoIsEnabled } = this.props
     return (
-      <ReactCSSTransitionGroup className={styles['transition-container']} transitionName="cross-fade" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+      <ReactCSSTransitionGroup className={styles['transition-container']} transitionName={styles['cross-fade']} transitionEnterTimeout={500} transitionLeaveTimeout={500}>
         <div className='transition-container' key={url}>
           <div style={{ backgroundImage: `url(${url})` }} className={styles.backdrop} />
-          <div className={styles['bottom-info-container']}>
-            <h1 onClick={this._openBackDropURL} className={styles.title}>{info.title}</h1>
-            <span className={styles.year}>{info.year}</span>
-            <p className={styles.rating}>{info.certification}</p>
-          </div>
+          {
+            infoIsEnabled ? (
+              <div className={styles['bottom-info-container']}>
+                <h1 onClick={this._openBackDropURL} className={styles.title}>{info.title}</h1>
+                <span className={styles.year}>{info.year}</span>
+                <p className={styles.rating}>{info.certification}</p>
+              </div>
+            ): null
+          }
         </div>
       </ReactCSSTransitionGroup>
     )
